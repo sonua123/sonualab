cc307d03417b56b7828fa2658f5e5226f5a6bb94
sonua123 <dudniksofia30@gmail.com>
2024-10-30 00:59:12 +0300

    2 лабраторная 2 попытка

--- task1.py
+++ task1.py
@@ -1,18 +1,13 @@
+from http.cookiejar import month
+
 money_capital = 20000
 salary = 5000
 spend = 6000
 increase = 0.05
+months= 0
 
-months = 0
-
-while True:
-
-    budget = salary + money_capital
-    if budget >= spend:
-        months += 1
-        money_capital -= (spend - salary)
-        spend *= (1 + increase)
-    else:
-        break
-
+while money_capital >= spend:
+  money_capital += salary - spend
+  spend += spend * increase
+  months += 1
 print("Количество месяцев, которое можно протянуть без долгов:", months)
