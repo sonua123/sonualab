51c663ad19cb0792b7e3126c185562ce07cbc2e8
sonua123 <dudniksofia30@gmail.com>
2025-02-04 21:33:04 +0300

    лабраторная 3 второй семестр

--- task1.py
+++ task1.py
@@ -1,29 +1,125 @@
-BOOKS_DATABASE = [
-    {"id": 1, "name": "test_name_1", "pages": 200},
-    {"id": 2, "name": "test_name_2", "pages": 400},
-]
-
-
 class Book:
-    def __init__(self, id_: int, name: str, pages: int):
-        self.id_ = id_
-        self.name = name
-        self.pages = pages
+    """Базовый класс книги."""
+
+    def __init__(self, name: str, author: str):
+        self._name = name  # защищенный атрибут
+        self._author = author  # защищенный атрибут
+
+    @property
+    def name(self) -> str:
+        """Возвращает название книги."""
+        return self._name
+
+    @property
+    def author(self) -> str:
+        """Возвращает автора книги."""
+        return self._author
 
     def __str__(self) -> str:
-        return f'Книга "{self.name}"'
+        return f"Книга: {self.name}, Автор: {self.author}"
 
     def __repr__(self) -> str:
-        return f"Book(id_={self.id_}, name={self.name!r}, pages={self.pages})"
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-if name == "__main__":
-    # Создаём список объектов Book из BOOKS_DATABASE
-    books = [Book(**book_data) for book_data in BOOKS_DATABASE]
+class PaperBook(Book):
+    """Дочерний класс бумажной книги."""
 
-    # Проверяем метод str
-    for book in books:
-        print(book)
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages  # Используем сеттер с проверками
 
-    # Проверяем метод repr
-    print(books)ниги с id=1)
+    @property
+    def pages(self) -> int:
+        """Возвращает количество страниц в книге."""
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        """Устанавливает количество страниц в книге с проверкой."""
+        if not isinstance(pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if pages <= 0:
+            raise ValueError("Количество страниц должно быть больше нуля")
+        self._pages = pages
+
+    def __str__(self) -> str:
+        return f"Бумажная книга: {self.name}, Автор: {self.author}, Страниц: {self.pages}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    """Дочерний класс аудиокниги."""
+
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration  # Используем сеттер с проверками
+
+    @property
+    def duration(self) -> float:
+        """Возвращает продолжительность книги."""
+        return self._durationclass
+        Book: """Базовый класс книги."""
+
+    def __init__(self, name: str, author: str):
+        self._name = name  # защищенный атрибут
+        self._author = author  # защищенный атрибут
+
+    @property
+    def name(self) -> str:
+        """Возвращает название книги."""
+        return self._name
+
+    @property
+    def author(self) -> str:
+        """Возвращает автора книги."""
+        return self._author
+
+    def __str__(self) -> str:
+        return f"Книга: {self.name}, Автор: {self.author}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    """Дочерний класс бумажной книги."""
+
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages  # Используем сеттер с проверками
+
+    @property
+    def pages(self) -> int:
+        """Возвращает количество страниц в книге."""
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        """Устанавливает количество страниц в книге с проверкой."""
+        if not isinstance(pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if pages <= 0:
+            raise ValueError("Количество страниц должно быть больше нуля")
+        self._pages = pages
+
+    def __str__(self) -> str:
+        return f"Бумажная книга: {self.name}, Автор: {self.author}, Страниц: {self.pages}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    """Дочерний класс аудиокниги."""
+
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration  # Используем сеттер с проверками
+
+    @property
+    def duration(self) -> float:
+        """Возвращает продолжительность книги."""
+        return self._duration
